5aa09c8409cb652c7121db94372162cd1de9be46
rpriyar93 <rpriyar93@gmail.com>
2018-06-13 20:03:07 -0700

    added some method

---
+++ src/github/githubtest.java
@@ -0,0 +1,13 @@
+package github;
+
+public class githubtest {
+	public static void main(String[] args) {
+		firstmethod();
+		}
+	
+		static  void firstmethod()
+		{
+		
+	System.out.println("first method");
+	}}
+
